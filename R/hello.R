37c50db92af6a5694465d40b4fe5392c21dd5edb
William Chase Hamby <williamchasehamby@yahoo.com>
2024-12-10 17:25:51 -0500

    R File

--- R/hello.R
+++ R/hello.R
@@ -40,7 +40,7 @@ colSums(is.na(combined_data))
 # I ran this code to clean up duplicates and now I am back to a good environment state
 # These snippets fix my oopsies
 
-#CSV File
+# CSV File
 combined_data <- read_csv("combined_nfl_kicking_data.csv")
 print(head(combined_data))
 combined_data <- combined_data %>%
@@ -49,28 +49,44 @@ print(paste("Updated row count:", nrow(combined_data)))
 write_csv(combined_data, "combined_nfl_kicking_data.csv")
 print("Cleaned data saved to 'combined_nfl_kicking_data.csv'.")
 
-#Local Table
+# Local Table
 combined_data <- combined_data %>%
   filter(Season != "Combined Data")
 print(paste("Updated row count:", nrow(combined_data)))
 print(head(combined_data))
 
-#Bring back Local Table with new session
+# Bring back Local Table with new session
 library(readr)
 combined_data <- read_csv("combined_nfl_kicking_data.csv")
 print(head(combined_data))
 View(combined_data)
 
-#Google Sheets
+# Google Sheets
 sheet_url <- "https://docs.google.com/spreadsheets/d/1pXzH4KFw3CzzTJlI_HMbqlihTzCxhxR-a1V1eYV1gxo/edit?usp=sharing"
 sheet_write(combined_data, ss = sheet_url, sheet = "Combined Data")
 print("Corrected data without 'Combined Data' rows updated in Google Sheets.")
 
+# Forgot some teams changed their name and/or moved locations
+library(dplyr)
+combined_data <- combined_data %>%
+  mutate(Tm = case_when(
+    Tm == "Washington Redskins" ~ "Washington Commanders",
+    Tm == "Washington Football Team" ~ "Washington Commanders",
+    Tm == "Oakland Raiders" ~ "Las Vegas Raiders",
+    TRUE ~ Tm
+  ))
+print("Updated Team Names:")
+print(unique(combined_data$Tm))
+write_csv(combined_data, "combined_nfl_kicking_data.csv")
+library(googlesheets4)
+sheet_url <- "https://docs.google.com/spreadsheets/d/1pXzH4KFw3CzzTJlI_HMbqlihTzCxhxR-a1V1eYV1gxo/edit?usp=sharing"
+sheet_write(combined_data, ss = sheet_url, sheet = "Combined Data")
 
-#Data Analysis
+
+# Data Analysis
 
 
-#Overall FGA vs FGM (By Season).pdf
+# Overall FGA vs FGM (By Season).pdf
 library(ggplot2)
 season_summary <- combined_data %>%
   group_by(Season) %>%
@@ -97,3 +113,5 @@ ggplot(season_summary, aes(x = Season)) +
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
 
+
+# Overall FGA vs FGM (By Team/Season).pdf
